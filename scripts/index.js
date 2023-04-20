7418d4cf132539963d3df9cb636e03dea0ff755a
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-04-20 15:29:12 +0200

    Fix css line game page

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const sums = document.querySelectorAll('.sum');
-    
-    let winWidth = window.innerWidth;
-    let winHeight = window.innerHeight;
+const sums = document.querySelectorAll('.sum');
+
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+for (let i = 0; i < sums.length; i++) {
+  let thisSum = sums[i];
   
-    for ( let i=0; i < sums.length; i++ ) {
-      let thisSum = sums[i];
-      
-      let randomTop = Math.floor(getRandomNumber(100, winHeight));
-      let randomLeft = Math.floor(getRandomNumber(10, winWidth));
-      
-      thisSum.style.top = randomTop +"px";
-      thisSum.style.left = randomLeft +"px";
-    }
+  let randomTop = Math.floor(getRandomNumber(0, winHeight));
+  let randomLeft = Math.floor(getRandomNumber(0, winWidth));
   
-    function getRandomNumber(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-});
+  thisSum.style.top = randomTop + "px";
+  thisSum.style.left = randomLeft + "px";
+  
+  setInterval(function() {
+    randomTop = Math.floor(getRandomNumber(0, winHeight));
+    randomLeft = Math.floor(getRandomNumber(0, winWidth));
+  
+    thisSum.style.top = randomTop + "px";
+    thisSum.style.left = randomLeft + "px";
+  }, 2500);
+}
